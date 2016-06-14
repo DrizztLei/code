fd379363bf065b88113c740d81485982dd964dc8
ElvisLouis <elvis.linuxer@gmail.com>
2016-06-14 18:33:21 +0800

    update in Tue Jun 14 18:33:21 CST 2016

--- work/C#/EVA/EVA/EVA/bin/Debug/eva.d
+++
@@ -1,2 +0,0 @@
-/home/elvis/work/C\#/EVA/EVA/EVA/bin/Debug/eva.o: \
- /home/elvis/work/C\#/EVA/EVA/EVA/eva.cpp
--- work/C#/EVA/EVA/EVA/eva.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(int argc , char ** argv){
-	using namespace std;
-	cout << "Hello EVA " << endl;
-	return 0;
-}
